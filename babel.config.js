acbafbc4f3c817b8c37b292d9c5834c7d63a02b5
xuqinggang <tsinggun@foxmail.com>
2023-03-20 09:39:47 +0800

    chore:add antd

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: ["@vue/cli-plugin-babel/preset"],
+    plugins: [
+      [
+        "import",
+        {
+          libraryName: "antd",
+          libraryDirectory: "es",
+          style: "css",
+        },
+      ],
+    ],
+  }
--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Button } from 'antd';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Button>Login</Button>
   )
 }
