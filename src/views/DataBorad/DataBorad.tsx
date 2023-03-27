4df6a120f150194e64f7ba8f672054bbe99359cf
xuqinggang <tsinggun@foxmail.com>
2023-03-27 10:59:20 +0800

    feat:add calculator

--- src/views/DataBorad/DataBorad.tsx
+++ src/views/DataBorad/DataBorad.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import TodoList from '@/Components/TodoList/TodoList'
-
+import Calculator from '@/Components/Calculator'
 export default function DataBorad() {
   return (
     <div>
       <TodoList />
+      <Calculator />
     </div>
   )
 }
